88e1cc13e71fb6a83e894d02f748a7cb063cc98a
Jeffrey Labonski <jlabonski@chariotsolutions.com>
2017-02-08 16:36:17 -0500

    Added additional weirdness, including a small Duff's Device.

--- src/app/model/dog.ts
+++ src/app/model/dog.ts
@@ -24,4 +24,41 @@ export class Dog {
 
   }
 
+  The crew was complete: it included a Boots—
+    A maker of Bonnets and Hoods—
+  A Barrister, brought to arrange their disputes—
+    And a Broker, to value their goods.
+
+
+  Please note that this really shouldn't compile.
+
+}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#pragma message "Compiling " __FILE__ "..."
+
+int main(void)
+{
+
+  send(to, from, count)
+  register short *to, *from;
+  register count;
+  {
+    register n = (count + 7) / 8;
+    switch (count % 8) {
+    case 0: do { *to = *from++;
+    case 7:      *to = *from++;
+    case 6:      *to = *from++;
+    case 5:      *to = *from++;
+    case 4:      *to = *from++;
+    case 3:      *to = *from++;
+    case 2:      *to = *from++;
+    case 1:      *to = *from++;
+              } while (--n > 0);
+    }
+  }
+
+  return EXIT_SUCCESS;
 }
