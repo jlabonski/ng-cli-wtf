1fa6b7740b987b68b85cb0c30de3e5b2305093b7
Jeffrey Labonski <jlabonski@chariotsolutions.com>
2017-02-08 16:30:10 -0500

    Added in weirdness.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Dog } from './model/dog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  public ngOnInit(): void {
+
+    const foo: any = {
+      name: 'Rover',
+      age: 4,
+      hasShots: true
+    };
+
+    var bar = foo as Dog;
+
+    console.log(bar.age);
+  }
+
 }
---
+++ src/app/model/dog.ts
@@ -0,0 +1,27 @@
+export class Dog {
+
+  public age: number;
+  //public hasShots: boolean;
+
+  constructor(public name: string){}
+
+  public bark(): string {
+    return "Woof";
+  }
+
+  public invalid(): void {
+
+
+    "Just the place for a Snark!" the Bellman cried,
+      As he landed his crew with care;
+    Supporting each man on the top of the tide
+      By a finger entwined in his hair.
+
+    "Just the place for a Snark! I have said it twice:
+      That alone should encourage the crew.
+    Just the place for a Snark! I have said it thrice:
+      What I tell you three times is true."
+
+  }
+
+}
